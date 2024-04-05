db3b77152bbb55834f7d47402b7a5852e52c56cc
valebaut <valebautista98@gmail.com>
2024-04-05 11:42:20 -0400

    HV code

---
+++ CoralHV_BEM2024.R
@@ -0,0 +1,128 @@
+#' """ NEMC Time-Series Hypervolumes 
+#'     @authors: Coral PR Team 
+#'     date: """3/7/24
+
+library(tidyverse)
+library(hypervolume)
+library(viridis)
+library(installr)
+
+
+# load trait data
+df_tr = read_csv('TraitTable.csv') |> 
+  drop_na()
+
+# load benthic data
+df_ben = read.csv('benthicdata_PR,csv')|> 
+rename(year = YEAR, region = REGION) |>
+  filter(`SITE NAME` %in% c("Carlos Rosario",  "Dakiti", "Cayo Diablo (2016)",  "Palominitos (2016)", "Palominos (2016)", "Canal Luis Pena" )) |> 
+  filter(`YEAR` %in% c(2016, 2018, 2021, 2023))|>
+  filter(species %in% unique(df_tr$species)) |> 
+  pivot_wider(names_from =species, values_from = percentcover, values_fill = 0) |> 
+  pivot_longer(`Acropora cervicornis`:`Porites furcata`, 
+               names_to = 'species', values_to = 'percentcover') |> 
+  group_by(species, region) |> 
+  summarize(pc = mean(percentcover),
+            pc_sd = sd(percentcover),
+            .groups = 'drop') |> 
+  filter(pc > 0) 
+
+# make the hvs for random cover and avg traits----
+reps = 100
+
+set.seed(14)
+df = df_ben |> 
+  left_join(df_tr, by = 'species') |> 
+  slice(rep(1:n(), each=reps))|> 
+  mutate(i = rep(1:reps, times=nrow(df_ben)),
+         percentcover = truncnorm::rtruncnorm(1, a = 0.001, b = 100,
+                                              mean = pc, sd = pc_sd),
+         # `chlorophyll a` = truncnorm::rtruncnorm(1,
+         #                                              mean = `mean_chlorophyll a`,
+         #                                              sd = `sd2_chlorophyll a`),
+         `corallite diameter` = `mean_corallite diameter`,
+         `growth rate` = `mean_growth rate`,
+         `skeletal density` = `mean_skeletal density`,
+         `symbiodinium density` = `mean_symbiodinium density`,
+         `colony maximum diameter` = `mean_colony maximum diameter`) |> 
+  select(region, i:`colony maximum diameter`) |> 
+  group_by(i) |> 
+  mutate(across(`corallite diameter`:`colony maximum diameter`, scale)) |> 
+  group_by(region, i) |> 
+  nest(weight = percentcover, data = `corallite diameter`:`colony maximum diameter`) |> 
+  mutate(hv = map2(data,weight, \(data,weight) hypervolume_gaussian(data, 
+                                                                    name = paste(region,i,sep = '_'),
+                                                                    weight = weight$percentcover,
+                                                                    samples.per.point = 1000,
+                                                                    kde.bandwidth = estimate_bandwidth(data), 
+                                                                    sd.count = 3, 
+                                                                    quantile.requested = 0.95, 
+                                                                    quantile.requested.type = "probability", 
+                                                                    chunk.size = 1000, 
+                                                                    verbose = F)),
+         hv_size = map_dbl(hv, \(hv) get_volume(hv)),
+         centroid = map(hv, \(hv) get_centroid(hv)))
+
+
+saveRDS(df, 'data/region_hvs_randCov_avgTr.rds')
+
+df |> select(region, i, hv_size, centroid) |> 
+  unnest_wider(centroid) |> 
+  write_csv('data/region_hvs_randCov_avgTr.csv')
+# will be too big for github
+
+# # plot size across region
+# ggplot(df, aes(region, hv_size))+
+#   geom_boxplot()
+
+
+# comparison of across region (change df_y to gb)
+df_reg = tibble(region1 = unique(df$region),
+                region2 = unique(df$region)) |> 
+  expand(region1,region2)
+
+df_reg = df_reg[!duplicated(t(apply(df_reg,1,sort))),] %>% 
+  filter(!(region1 == region2))
+
+df_iterations = tibble(region1 = rep(df_reg$region1, times = reps),
+                       region2 = rep(df_reg$region2, times = reps),
+                       i = rep(1:reps, each = length(df_reg$region1))) 
+
+# years to make 
+df_1 = df|> 
+  select(region1 = region, hv1 = hv, hv1_size = hv_size, i = i)
+
+df_2 = df |> 
+  select(region2 = region, hv2 = hv, hv2_size = hv_size, i = i)
+
+# create large df to store all data
+
+# tibble(BASIN = rep(unique(df$BASIN),
+#             each = nrow(df_y)),
+# y1 = rep(df_y$y1, times = length(unique(df$BASIN))),
+# y2 = rep(df_y$y2, times = length(unique(df$BASIN))))
+
+df_ov = df_iterations |> 
+  inner_join(df_1, by = c('region1', 'i')) |> 
+  inner_join(df_2, by = c('region2', 'i')) |> 
+  mutate(set = map2(hv1,hv2, \(hv1, hv2) hypervolume_set(hv1, hv2, check.memory = F, verbose = F)),
+         ov = map(set, \(set) hypervolume_overlap_statistics(set)),
+         dist_cent = map2_dbl(hv1, hv2, \(hv1,hv2) hypervolume_distance(hv1, hv2, type = 'centroid', check.memory=F)),
+         size_ratio = hv1_size/hv2_size,
+         i = i) |> 
+  unnest_wider(ov) |> 
+  select(region1, region2, hv1_size, hv2_size, size_ratio,
+         jaccard, sorensen,uniq_y1 = frac_unique_1, uniq_y2 = frac_unique_2, 
+         dist_cent)
+
+#saveRDS(df_ov, "data/region_ov.rds")
+write_csv(df_ov, "data/region_ov_randCov_avgTr.csv")
+
+df_avg = df_ov |> 
+  group_by(region1,region2) |>
+  summarize(across(hv1_size:dist_cent, mean))
+
+ggplot(df_avg, aes(region1, region2, fill = sorensen))+
+  geom_tile()+
+  scale_fill_viridis(limits = c(0,1))
+
